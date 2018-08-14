d32cd565867aa944d92ade1ccad8b1ce29dba6c3
Chris <chrislo.7@live.com>
2018-08-14 18:39:35 +0000

    added .ready(...)

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,18 @@
-const textarea = document.getElementById("textarea");
-var count = 140;
+$(document).ready(function() {
+  const textarea = document.getElementById("textarea");
+  var count = 140;
 
-var countChar = function() {
-  const counter = $(this).siblings(".counter");
-  let char = this.value.length;
-  let val = count - char;
-  if (val < 0) {
-    counter.css("color", "red");
-  } else {
-    counter.css("color", "black");
-  }
-  counter.text(val);
-};
+  var countChar = function() {
+    const counter = $(this).siblings(".counter");
+    let char = this.value.length;
+    let val = count - char;
+    if (val < 0) {
+      counter.css("color", "red");
+    } else {
+      counter.css("color", "black");
+    }
+    counter.text(val);
+  };
 
-textarea.addEventListener("keyup", countChar);
+  textarea.addEventListener("keyup", countChar);
+});

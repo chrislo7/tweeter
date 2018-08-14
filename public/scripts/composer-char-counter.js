5315719a1a028ad4b37256f5ba621b069857d902
Chris <chrislo.7@live.com>
2018-08-14 18:22:23 +0000

    if character count under 0, it will be displayed as red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,14 @@ const textarea = document.getElementById('textarea');
 var count = 140;
 
 var countChar = function() {
-
   var char = this.value.length;
-  $(this).siblings('span')[0].innerHTML = count - char;
-  if(counter.innerHTML < 0){}
+  var num = count - char;
+  if (num < 0) {
+    $(this).siblings('.counter').css('color', 'red')
+  } else {
+    $(this).siblings('.counter').css('color', 'black')
+  }
+  $(this).siblings('.counter').text(num);
 }
 
 textarea.addEventListener('keyup', countChar)

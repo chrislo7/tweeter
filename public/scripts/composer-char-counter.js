c62a71c7c963c63a6ad6392bae67edac12361b22
Chris <chrislo.7@live.com>
2018-08-14 18:06:07 +0000

    used jquery traversing instead of getElementById

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,11 @@
-var textarea = document.getElementById('textarea');
-var counter = document.getElementById('counter');
+const textarea = document.getElementById('textarea');
 var count = 140;
 
 var countChar = function() {
-  var char = textarea.value.length;
-  counter.innerHTML = count - char;
+
+  var char = this.value.length;
+  $(this).siblings('span')[0].innerHTML = count - char;
+  if(counter.innerHTML < 0){}
 }
 
 textarea.addEventListener('keyup', countChar)

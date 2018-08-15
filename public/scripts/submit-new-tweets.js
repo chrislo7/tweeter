729675d0cae4941ba8c177cd4eebc6b09d2fb0d9
Chris <chrislo.7@live.com>
2018-08-15 20:31:32 +0000

    display error msg next to tweet button when error occurs

--- public/scripts/submit-new-tweets.js
+++ public/scripts/submit-new-tweets.js
@@ -3,9 +3,11 @@
   $( "input" ).click(function( event ) {
     event.preventDefault();
     if (!$('textarea').val()) {
-      alert("You haven't tweeted anything yet!")
+      $("err").text('Nothing entered!')
+      $("err").fadeIn();
     } else if ($('textarea').val().length > 140){
-      alert("Your tweet contains too many characters!")
+      $("err").text("Your tweet contains too many characters!")
+      $("err").fadeIn();
     } else {
       let serializedTweet = $("textarea").serialize();
       let content = $("textarea").val();
@@ -16,6 +18,7 @@
         success: function(){
           $(".old-tweets").remove();
           loadTweets();
+          $('err').css("display", "none")
           $('textarea').val('');
           $('.counter').text(140);
         }
